e72a96575a8c8ab52c6d2180b4787a06781d1ae9
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-10-03 23:09:47 +0600

    Product count feature added

--- js/shopping.js
+++ js/shopping.js
@@ -1,3 +1,18 @@
-function addToCart() {
-  console.log("product clicked");
+const cartArray = [];
+
+function addToCart(element) {
+  //   console.log(element.parentNode.parentNode.children);
+  //   console.log(element.parentNode.parentNode.children[0].innerText);
+  //   console.log(element.parentNode.parentNode.children[1].children[0].innerText);
+
+  const productName = element.parentNode.parentNode.children[0].innerText;
+  const productPrice =
+    element.parentNode.parentNode.children[1].children[0].innerText;
+  const objProduct = {
+    productName: productName,
+    productPrice: parseFloat(productPrice),
+  };
+
+  cartArray.push(objProduct);
+  document.getElementById("Total-added-product").innerText = cartArray.length;
 }
